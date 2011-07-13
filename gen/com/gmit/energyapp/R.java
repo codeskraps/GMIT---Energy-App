88ffa1189e10bec3455e25f81082742e16e4025a
Carles Sentis <heepie202@gmail.com>
2011-07-13 04:31:28 +0100

    Wed Jul 13 04:31

--- gen/com/gmit/energyapp/R.java
+++ gen/com/gmit/energyapp/R.java
@@ -34,9 +34,11 @@ public final class R {
         public static final int home=0x7f020010;
         public static final int home_small=0x7f020011;
         public static final int icon=0x7f020012;
-        public static final int solar=0x7f020013;
-        public static final int solar_panels_lvl_2=0x7f020014;
-        public static final int solar_panels_system_overview_lvl_3=0x7f020015;
+        public static final int pin=0x7f020013;
+        public static final int solar=0x7f020014;
+        public static final int solar_panels_lvl_2=0x7f020015;
+        public static final int solar_panels_system_overview_lvl_3=0x7f020016;
+        public static final int solar_panels_system_overview_lvl_3_pins=0x7f020017;
     }
     public static final class id {
         public static final int btnBiomassOne=0x7f080000;
@@ -54,13 +56,13 @@ public final class R {
         public static final int btnHomeTwo=0x7f08000c;
         public static final int btnSolarOne=0x7f080013;
         public static final int btnSolarTwo=0x7f080014;
-        public static final int imageView1=0x7f080015;
         public static final int img_about=0x7f080012;
         public static final int img_biomass=0x7f08000f;
         public static final int img_gasboiler=0x7f080010;
         public static final int img_heatpump=0x7f08000e;
         public static final int img_heattransfer=0x7f080011;
         public static final int img_solar=0x7f08000d;
+        public static final int img_solar_overview=0x7f080015;
         public static final int itemGasBoiler=0x7f08001b;
         public static final int itemHRVU=0x7f08001a;
         public static final int itemHeatPump=0x7f080019;
@@ -91,34 +93,34 @@ public final class R {
         public static final int app_name=0x7f050000;
         /**  Biomass 
          */
-        public static final int biomassLevel2Text=0x7f05001a;
+        public static final int biomassLevel2Text=0x7f05002e;
         public static final int biomass_activity=0x7f050004;
-        public static final int btnBiomassOne=0x7f05001b;
-        public static final int btnBiomassThree=0x7f05001d;
-        public static final int btnBiomassTwo=0x7f05001c;
-        public static final int btnGasBoilerOne=0x7f05001f;
-        public static final int btnGasBoilerTwo=0x7f050020;
-        public static final int btnHeatOne=0x7f050017;
-        public static final int btnHeatThree=0x7f050019;
-        public static final int btnHeatTransferOne=0x7f050022;
-        public static final int btnHeatTransferThree=0x7f050024;
-        public static final int btnHeatTransferTwo=0x7f050023;
-        public static final int btnHeatTwo=0x7f050018;
+        public static final int btnBiomassOne=0x7f05002f;
+        public static final int btnBiomassThree=0x7f050031;
+        public static final int btnBiomassTwo=0x7f050030;
+        public static final int btnGasBoilerOne=0x7f050033;
+        public static final int btnGasBoilerTwo=0x7f050034;
+        public static final int btnHeatOne=0x7f05002b;
+        public static final int btnHeatThree=0x7f05002d;
+        public static final int btnHeatTransferOne=0x7f050036;
+        public static final int btnHeatTransferThree=0x7f050038;
+        public static final int btnHeatTransferTwo=0x7f050037;
+        public static final int btnHeatTwo=0x7f05002c;
         public static final int btnHomeOne=0x7f050011;
         public static final int btnHomeTwo=0x7f050012;
         public static final int btnSolarOne=0x7f050014;
         public static final int btnSolarTwo=0x7f050015;
         /**  Gas Boiler 
          */
-        public static final int gasboilerLevel2Text=0x7f05001e;
+        public static final int gasboilerLevel2Text=0x7f050032;
         public static final int gasboiler_activity=0x7f050005;
         /**  HeatPump 
          */
-        public static final int heatpumpLevel2Text=0x7f050016;
+        public static final int heatpumpLevel2Text=0x7f05002a;
         public static final int heatpump_activity=0x7f050003;
         /**  Heat Transfer 
          */
-        public static final int heattransferLevel2Text=0x7f050021;
+        public static final int heattransferLevel2Text=0x7f050035;
         public static final int heattransfer_activity=0x7f050006;
         /**  HOME 
          */
@@ -133,10 +135,30 @@ public final class R {
         public static final int menuQuit=0x7f05000e;
         public static final int menuSolar=0x7f050009;
         public static final int menuWind=0x7f05000f;
-        public static final int ok=0x7f050025;
+        public static final int ok=0x7f050039;
         /**  Solar 
          */
         public static final int solarLevel2Text=0x7f050013;
+        public static final int solarMessagePin1=0x7f050020;
+        public static final int solarMessagePin10=0x7f050029;
+        public static final int solarMessagePin2=0x7f050021;
+        public static final int solarMessagePin3=0x7f050022;
+        public static final int solarMessagePin4=0x7f050023;
+        public static final int solarMessagePin5=0x7f050024;
+        public static final int solarMessagePin6=0x7f050025;
+        public static final int solarMessagePin7=0x7f050026;
+        public static final int solarMessagePin8=0x7f050027;
+        public static final int solarMessagePin9=0x7f050028;
+        public static final int solarTitlePin1=0x7f050016;
+        public static final int solarTitlePin10=0x7f05001f;
+        public static final int solarTitlePin2=0x7f050017;
+        public static final int solarTitlePin3=0x7f050018;
+        public static final int solarTitlePin4=0x7f050019;
+        public static final int solarTitlePin5=0x7f05001a;
+        public static final int solarTitlePin6=0x7f05001b;
+        public static final int solarTitlePin7=0x7f05001c;
+        public static final int solarTitlePin8=0x7f05001d;
+        public static final int solarTitlePin9=0x7f05001e;
         public static final int solar_activity=0x7f050001;
         public static final int solar_overview_activity=0x7f050002;
     }
--- src/com/gmit/energyapp/SolarOverviewActivity.java
+++ src/com/gmit/energyapp/SolarOverviewActivity.java
@@ -24,8 +24,22 @@ package com.gmit.energyapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 
 public class SolarOverviewActivity extends Activity {
+	private static final String TAG = SolarOverviewActivity.class.getSimpleName();
+	
+//	private static final short DIALOG_PIN_1 = 1;
+//	private static final short DIALOG_PIN_2 = 2;
+//	private static final short DIALOG_PIN_3 = 3;
+//	private static final short DIALOG_PIN_4 = 4;
+//	private static final short DIALOG_PIN_5 = 5;
+//	private static final short DIALOG_PIN_6 = 6;
+//	private static final short DIALOG_PIN_7 = 7;
+//	private static final short DIALOG_PIN_8 = 8;
+//	private static final short DIALOG_PIN_9 = 9;
+//	private static final short DIALOG_PIN_10 = 10;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,4 +47,38 @@ public class SolarOverviewActivity extends Activity {
 		setContentView(R.layout.solar_overview);
 	}
 
+//	@Override
+//    protected Dialog onCreateDialog(int id) {
+//        switch (id) {
+//        case DIALOG_PIN_1:
+//        	Log.d(TAG, "onCreateDialog 1");
+//        	return new AlertDialog.Builder(SolarOverviewActivity.this)
+//            //.setIcon(R.drawable.alert_dialog_icon)
+//            .setTitle(R.string.solarTitlePin1)
+//            .setMessage(R.string.solarMessagePin1)
+//            .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+//                public void onClick(DialogInterface dialog, int whichButton) {
+//
+//                    /* User clicked OK so do some stuff */
+//                }
+//            })
+//            .create();
+//        }
+//        return null;
+//	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		Log.d(TAG, "onTouchEvent started");
+		
+	    switch (event.getAction()) {
+	        case MotionEvent.ACTION_DOWN: 
+	        	float x = event.getX();
+	            float y = event.getY();
+	            
+	            Log.d(TAG, "X: " + x + ", Y: " + y);
+	            break;
+	    }
+		return super.onTouchEvent(event);
+	}
 }
